06dd5e6e6385765f5d5ef6138a1d6452584cdafe
wind <987625922@qq.com>
2019-04-24 15:08:26 +0800

    delect makexml.java

--- app/src/main/java/com/wind/androidlearn/TestActivity.java
+++ app/src/main/java/com/wind/androidlearn/TestActivity.java
@@ -1,13 +1,16 @@
 package com.wind.androidlearn;
 
-import android.Manifest;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.text.Html;
 import android.widget.TextView;
 
@@ -24,11 +27,12 @@ import com.wyt.zdf.myapplication.R;
  * Copyright (c) ： by WaiYuTong.版权所有.<br>
  * 待做事情：
  */
-public class TestActivity extends Activity  {
+public class TestActivity extends Activity {
     private TextView textView;
     private String picName = "networkPic.jpg";
-//    private NetworkImageGetter mImageGetter;
-    String str = "<img src=\"http://image.shentong360.com/upload/img/20180906/0547198eeab47323beabc0205a456917.jpg\"><br>";
+    //    private NetworkImageGetter mImageGetter;
+    String str = "<img src=\"http://image.shentong360" +
+            ".com/upload/img/20180906/0547198eeab47323beabc0205a456917.jpg\"><br>";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,20 +42,53 @@ public class TestActivity extends Activity  {
         MyImageGetter myImageGetter = new MyImageGetter();
         textView.setText(Html.fromHtml(str, myImageGetter, null));
 
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+            }
+        };
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                handler.sendEmptyMessage(1);
+            }
+        }.start();
+
+        Handler handler1 = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+            }
+        };
+        handler1.post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
     }
 
     class MyDrawableWrapper extends BitmapDrawable {
         private Drawable drawable;
+
         MyDrawableWrapper() {
         }
+
         @Override
         public void draw(Canvas canvas) {
             if (drawable != null)
                 drawable.draw(canvas);
         }
+
         public Drawable getDrawable() {
             return drawable;
         }
+
         public void setDrawable(Drawable drawable) {
             this.drawable = drawable;
         }
@@ -78,15 +115,17 @@ public class TestActivity extends Activity  {
 
     class BitmapTarget extends SimpleTarget<Bitmap> {
         private final MyDrawableWrapper myDrawable;
+
         public BitmapTarget(MyDrawableWrapper myDrawable) {
             this.myDrawable = myDrawable;
         }
+
         @Override
         public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
             Drawable drawable = new BitmapDrawable(getResources(), resource);
             //获取原图大小
-            int width=drawable.getIntrinsicWidth() ;
-            int height=drawable.getIntrinsicHeight();
+            int width = drawable.getIntrinsicWidth();
+            int height = drawable.getIntrinsicHeight();
             //自定义drawable的高宽, 缩放图片大小最好用matrix变化，可以保证图片不失真
             drawable.setBounds(0, 0, 500, 500);
             myDrawable.setBounds(0, 0, 500, 500);
@@ -96,6 +135,13 @@ public class TestActivity extends Activity  {
         }
     }
 
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+        }
+    };
 
 
 }
--- app/src/main/java/com/wind/androidlearn/constants/ARouterConfig.java
+++ app/src/main/java/com/wind/androidlearn/constants/ARouterConfig.java
@@ -1,7 +1,5 @@
 package com.wind.androidlearn.constants;
 
-import com.alibaba.android.arouter.facade.annotation.Route;
-
 public interface ARouterConfig {
     String AROUTER = "/test/activity";
 }
