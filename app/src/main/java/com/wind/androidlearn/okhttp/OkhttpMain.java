6dd20e989614ce16597d608569060b6b371b8160
wind <987625922@qq.com>
2021-01-08 13:43:53 +0800

    面试笔记代码

--- app/src/main/java/com/wind/androidlearn/okhttp/OkhttpMain.java
+++ app/src/main/java/com/wind/androidlearn/okhttp/OkhttpMain.java
@@ -7,8 +7,12 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import okhttp3.Cache;
+import okhttp3.CacheControl;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -24,10 +28,21 @@ class OkhttpMain {
     public static final String TAG = "okhttp源码解析";
 
     public static void main(String[] args) {
+        //okhttp的缓存
+        File cacheFile = new File("缓存文件的path", "cache");
+        int cacheSeize = 10 * 1024 * 1024;
+        Cache cache = new Cache(cacheFile, cacheSeize);
+        //缓存的设置
+        final CacheControl.Builder builder = new CacheControl.Builder();
+        //设置最大有效时间为100毫秒
+        builder.maxAge(100, TimeUnit.MILLISECONDS);
+        CacheControl cacheControl = builder.build();
+        //开始网络请求
         String url = "http://wwww.baidu.com";
-        OkHttpClient okHttpClient = new OkHttpClient();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().cache(cache).build();
         final Request request = new Request.Builder()
                 .url(url)
+                .cacheControl(cacheControl)
                 .get() //默认就是GET请求，可以不写
                 .build();
         Call call = okHttpClient.newCall(request);
